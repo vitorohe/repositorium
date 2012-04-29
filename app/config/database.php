61d08b45273c854e1a8542c1fce0790fd6dfe072
Victor Rojas <rojas@aimerchant.com>
2012-04-29 15:38:48 -0300

    database pass changed

--- app/config/database.php
+++ app/config/database.php
@@ -78,7 +78,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'root',
+		'password' => '',
 		'database' => 'repositorium',
 		'prefix' => '',
 	);
@@ -88,7 +88,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'root',
+		'password' => '',
 		'database' => 'repositorium',
 		'prefix' => '',
 	);
