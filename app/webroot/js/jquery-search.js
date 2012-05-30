a0de0c30f5e17f6c04fea123218cf4a6cf9ba7ec
Eduardo Frias <alexislalo@hotmail.com>
2012-05-29 21:08:00 -0400

    asdf

--- app/webroot/js/jquery-search.js
+++ app/webroot/js/jquery-search.js
@@ -5,7 +5,9 @@
          in the table.
         */
         $('#msg').html('<p style="padding:5px;">Enter a search term to start filtering.</p>');
+        $('#msgc').html('<p style="padding:5px;">Enter a search term to start filtering.</p>');
         $('#sortable1').html('');
+        $('#sortable3').html('');
 
         /* When the user enters a value such as "j" in the search box
          * we want to run the .get() function. */
@@ -21,7 +23,7 @@
 
                 /* Fire the .get() method for and pass the searchVal data to the
                  * search-data.php file for retrieval */
-                $.get('/repositorium/index.php/criterias/autocomplete?searchData='+searchVal, function(returnData) {
+                $.get('/repositorium/index.php/criterias/autocomplete/?searchData='+searchVal+'&criterias='+$('#sortable2').text()+'&categories='+$('#sortable4').text(), function(returnData) {
                     /* If the returnData is empty then display message to user
                      * else our returned data results in the table.  */
                     if (!returnData) {
@@ -35,6 +37,36 @@
             } else {
                 $('#msg').html('<p style="padding:5px;">Enter a search term to start filtering.</p>');
                 $('#sortable1').html('');
+            }
+
+        });
+        
+        $('#searchDatac').keyup(function() {
+
+            /* Get the value of the search input each time the keyup() method fires so we
+             * can pass the value to our .get() method to retrieve the data from the database */
+            var searchVal = $(this).val();
+
+            /* If the searchVal var is NOT empty then check the database for possible results
+             * else display message to user */
+            if(searchVal !== '') {
+
+                /* Fire the .get() method for and pass the searchVal data to the
+                 * search-data.php file for retrieval */
+                $.get('/repositorium/index.php/categories/autocomplete/?searchData='+searchVal+'&categories='+$('#sortable4').text()+'&criterias='+$('#sortable2').text(), function(returnData) {
+                    /* If the returnData is empty then display message to user
+                     * else our returned data results in the table.  */
+                    if (!returnData) {
+                        $('#msgc').html('<p style="padding:5px;">Search term entered does not return any data.</p>');
+                        $('#sortable3').html('');
+                    } else {
+                        $('#sortable3').html(returnData);
+                        $('#msgc').html('');
+                    }
+                });
+            } else {
+                $('#msgc').html('<p style="padding:5px;">Enter a search term to start filtering.</p>');
+                $('#sortable3').html('');
             }
 
         });
