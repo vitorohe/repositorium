0fe9c5f84cd4715e8989d99afa09e12101f7f93f
Victor Rojas <vitorohe@gmail.com>
2012-06-19 11:42:39 -0400

    fix redirect admin points

--- app/controllers/admin_points_controller.php
+++ app/controllers/admin_points_controller.php
@@ -45,21 +45,21 @@ class AdminPointsController extends AppController {
 
   	$this->paginate['User']['limit'] = $this->Session->check('User.limit') ? $this->Session->read('User.limit') : 5;
   	$this->paginate['User']['recursive'] = -1;
-  	
+
   }
-  
+
   /* List users and their points */
   function listUsersPoints(){
   	$user = $this->getConnectedUser();
   	$rep = $this->getCurrentRepository();
-  	
+
   	$this->set_paginate();
   	$users = $this->paginate();
-  	
+
   	$crit_list = $this->Criteria->getCriteriasForExpert($user['User']['id']);
 	$criteria_id = array_keys($crit_list);
-	$criteria_id = $criteria_id[0]; 
-  	
+	$criteria_id = $criteria_id[0];
+
   	$current = 'points';
   	$menu = 'menu_expert';
   	$criteria_n = $this->Session->check('User.criteria') && $this->Session->read('User.criteria') != 0 ?
@@ -67,37 +67,37 @@ class AdminPointsController extends AppController {
   	$criteria_list = $crit_list;
   	$limit = $this->Session->check('User.limit') ? $this->Session->read('User.limit') : $this->paginate['User']['limit'];
   	//$ordering = $this->Session->read('CriteriasDocument.order') ? $this->Session->read('CriteriasDocument.order') : $this->_arrayToStr($this->paginate['Criteria']['order']);
-  	
+
   	$crit_user = $this->CriteriasUser->getUsersPoints($criteria_n);
-  	
+
   	$title = 'points';
-  	
+
 	$this->set(compact('current', 'rep', 'menu', 'limit', 'criteria_n', 'criteria_list', 'title', 'users', 'crit_user'));
   }
-  
-  
+
+
   /*Add points to the user in a criteria*/
   function add_points(){
   	if(empty($this->data['User']['points'])){
   		$this->Session->setFlash('The quantity of points cannot be empty');
-  		$this->redirect($this->referer());
+  		$this->redirect(array('controller' => 'admin_points', 'action' => 'listUsersPoints'));
   	}
   	if(!is_numeric($this->data['User']['points'])){
   		$this->Session->setFlash('The quantity of points must be numbers');
-  		$this->redirect($this->referer());
+  		$this->redirect(array('controller' => 'admin_points', 'action' => 'listUsersPoints'));;
   	}
-  	
+
   	$user = $this->getConnectedUser();
-  	
+
   	$crit = $this->Criteria->getCriteriabyUser($this->data['Criteria']['id'], $user['User']['id']);
-  	 
+
   	if(empty($crit)){
   		$this->Session->setFlash('Permission Denied', 'flash_errors');
-  		$this->redirect($this->referer());
+  		$this->redirect('/');
   	}
-  	
+
   	$crit_user = $this->Criteria->getCriteriabyUser($this->data['Criteria']['id'], $this->data['User']['id'], array(1, 2));
-  	
+
   	if(empty($crit_user)){
   		$row['criteria_id'] = $this->data['Criteria']['id'];
   		$row['user_id'] = $this->data['User']['id'];
@@ -114,15 +114,15 @@ class AdminPointsController extends AppController {
   		if($row['score_obtained'] < 0)
   			$row['score_obtained'] = 0;
   	}
-  	
+
   	if($this->CriteriasUser->save($row)){
   		$this->Session->setFlash('Points successfully added to user', 'flash_green');
   	}
   	else{
   		$this->Session->setFlash('There was an error, please blame the developer', 'flash_errors');
   	}
-  	
-  	$this->redirect($this->referer());
+
+  	$this->redirect(array('controller' => 'admin_points', 'action' => 'listUsersPoints'));
   }
-  
+
 }
