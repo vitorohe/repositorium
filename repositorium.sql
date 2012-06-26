b8d2d723c687574f10774c8fc9075543015bcd09
Victor Rojas <vitorohe@gmail.com>
2012-06-25 22:10:42 -0400

    htaccess y dump sql

--- repositorium.sql
+++ repositorium.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 3.4.10.1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 07-05-2012 a las 23:04:28
--- Versión del servidor: 5.5.16
--- Versión de PHP: 5.3.8
+-- Tiempo de generación: 26-06-2012 a las 01:52:27
+-- Versión del servidor: 5.5.20
+-- Versión de PHP: 5.3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS `attachfiles` (
   `id` int(20) NOT NULL AUTO_INCREMENT COMMENT 'Identificador',
   `name` varchar(100) NOT NULL COMMENT 'Nombre del Documento',
   `location` varchar(100) NOT NULL COMMENT 'Ubicacion del Documento Adjunto en el Sistema',
+  `extension` varchar(6) NOT NULL,
   `activation_id` char(1) NOT NULL COMMENT 'Clave Foranea Activacions',
   `internalstate_id` char(1) NOT NULL COMMENT 'Clave Foranea Estadointernos',
   `document_id` int(15) NOT NULL COMMENT 'Clave Foranea Documentos',
@@ -59,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `attachfiles` (
   KEY `documento_adjuntos_01` (`activation_id`),
   KEY `documento_adjuntos_02` (`internalstate_id`),
   KEY `documento_adjuntos_03` (`document_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
 -- --------------------------------------------------------
 
@@ -78,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `categories` (
   KEY `categorias_01` (`activation_id`),
   KEY `categorias_02` (`internalstate_id`),
   KEY `categorias_03` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Categorias del Sistema' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Categorias del Sistema' AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -97,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `category_criterias` (
   KEY `categoria_criterios_02` (`internalstate_id`),
   KEY `categoria_criterios_03` (`category_id`),
   KEY `categoria_criterios_04` (`criteria_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Criterios que pertenecen a una Categoria' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Criterios que pertenecen a una Categoria' AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -117,35 +118,12 @@ CREATE TABLE IF NOT EXISTS `criterias` (
   `activation_id` char(1) NOT NULL COMMENT 'Clave Foranea Activacions',
   `internalstate_id` char(1) NOT NULL COMMENT 'Clave Foranea Estadointernos',
   `user_id` int(6) NOT NULL COMMENT 'Clave Foranea Usuarios (Creador del Criterio)',
+  `questions_quantity` int(6) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `criterios_01` (`activation_id`),
   KEY `criterios_02` (`internalstate_id`),
   KEY `criterios_03` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=37 ;
-
---
--- Volcado de datos para la tabla `criterias`
---
-
-INSERT INTO `criterias` (`id`, `name`, `question`, `upload_score`, `download_score`, `collaboration_score`, `register_date`, `register_ip`, `activation_id`, `internalstate_id`, `user_id`) VALUES
-(7, 'Buena redacciÃ³n', 'Â¿La conexiÃ³n de las ideas es buena?', 4, 3, 2, '0000-00-00 00:00:00', '', 'A', 'A', 1),
-(8, 'aaaaa', 'asd', 2, 1, 3, '0000-00-00 00:00:00', '', 'A', 'A', 1),
-(9, 'hola', 'hola', 2, 1, 3, '0000-00-00 00:00:00', '', 'A', 'A', 1),
-(15, 'mycrit', 'mycrit', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(16, 'sdfbgfsdgd', 'gdsgfsd', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(17, 'hola', 'chao', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(18, 'sdafsa', 'fasfsa', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(19, 'sdafsa12', 'fasfsa12', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(20, 'soyuncrit', 'sadgds', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(21, 'gdsfsagda', 'fsagsafg', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(22, 'hola12', 'sdafs', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(23, 'hola12asdf', 'sdafsdswa', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(31, 'HOLA123', 'CHAO!1', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(32, 'mycrit1', 'asd??', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(33, 'MNBV', 'POLIKJ', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 10),
-(34, 'sdgfda', 'sdgdsf', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 11),
-(35, 'safsa', 'fsags', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 11),
-(36, 'hjola', 'gholadfgs', 5, 10, 5, '0000-00-00 00:00:00', '', 'A', 'A', 11);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=82 ;
 
 -- --------------------------------------------------------
 
@@ -160,45 +138,15 @@ CREATE TABLE IF NOT EXISTS `criterias_documents` (
   `document_id` int(15) NOT NULL COMMENT 'Clave Foranea Documentos',
   `criteria_id` int(6) NOT NULL COMMENT 'Clave Foranea Criterios',
   `answer` int(2) NOT NULL DEFAULT '3' COMMENT 'Cumple o no el criterio',
+  `yes_eval` int(6) NOT NULL DEFAULT '0',
+  `no_eval` int(6) NOT NULL DEFAULT '0',
+  `total_eval` int(7) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `documento_criterios_01` (`activation_id`),
   KEY `documento_criterios_02` (`internalstate_id`),
   KEY `documento_criterios_03` (`document_id`),
   KEY `documento_criterios_04` (`criteria_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
-
---
--- Volcado de datos para la tabla `criterias_documents`
---
-
-INSERT INTO `criterias_documents` (`id`, `activation_id`, `internalstate_id`, `document_id`, `criteria_id`, `answer`) VALUES
-(1, 'A', 'A', 21, 7, 3),
-(2, 'A', 'A', 23, 7, 3),
-(3, 'A', 'A', 24, 7, 3),
-(4, 'A', 'A', 25, 9, 3),
-(5, 'A', 'A', 25, 7, 3),
-(6, 'A', 'A', 26, 7, 3),
-(7, 'A', 'A', 27, 7, 3),
-(8, 'A', 'A', 28, 9, 3),
-(9, 'A', 'A', 29, 8, 3),
-(10, 'A', 'A', 30, 8, 3),
-(11, 'A', 'A', 31, 7, 3),
-(12, 'A', 'A', 32, 8, 3),
-(13, 'A', 'A', 33, 8, 3),
-(14, 'A', 'A', 34, 8, 3),
-(15, 'A', 'A', 35, 7, 3),
-(16, 'A', 'A', 35, 9, 3),
-(17, 'A', 'A', 35, 8, 3),
-(18, 'A', 'A', 36, 9, 3),
-(19, 'A', 'A', 37, 33, 2),
-(20, 'A', 'A', 38, 32, 1),
-(21, 'A', 'A', 38, 31, 2),
-(22, 'A', 'A', 39, 33, 1),
-(23, 'A', 'A', 39, 32, 2),
-(24, 'A', 'A', 39, 31, 1),
-(25, 'A', 'A', 40, 32, 2),
-(26, 'A', 'A', 41, 33, 1),
-(27, 'A', 'A', 41, 31, 2);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=89 ;
 
 -- --------------------------------------------------------
 
@@ -222,19 +170,7 @@ CREATE TABLE IF NOT EXISTS `criterias_users` (
   KEY `usuario_criterios_04` (`user_id`),
   KEY `usuario_criterios_05` (`criteria_id`),
   KEY `usuario_criterios_06` (`quality_user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Usuarios asociados a los Criterios para almacenar el Puntaje' AUTO_INCREMENT=8 ;
-
---
--- Volcado de datos para la tabla `criterias_users`
---
-
-INSERT INTO `criterias_users` (`id`, `successful_evaluation`, `negative_evaluation`, `score_obtained`, `activation_id`, `internalstate_id`, `user_id`, `criteria_id`, `quality_user_id`) VALUES
-(2, 0, 0, 5, 'A', 'A', 10, 31, 1),
-(3, 0, 0, 0, 'A', 'A', 10, 32, 1),
-(4, 0, 0, 0, 'A', 'A', 10, 33, 1),
-(5, 0, 0, 0, 'A', 'A', 11, 34, 1),
-(6, 0, 0, 0, 'A', 'A', 11, 35, 1),
-(7, 0, 0, 0, 'A', 'A', 11, 36, 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Usuarios asociados a los Criterios para almacenar el Puntaje' AUTO_INCREMENT=55 ;
 
 -- --------------------------------------------------------
 
@@ -248,7 +184,7 @@ CREATE TABLE IF NOT EXISTS `documents` (
   `description` text NOT NULL COMMENT 'Descripcion del Documento',
   `register_date` datetime NOT NULL COMMENT 'Fecha de la Subida del Documento',
   `register_ip` varchar(25) NOT NULL COMMENT 'IP de la Subida del Documento',
-  `registration_date` datetime NOT NULL COMMENT 'Fecha del Alta del Documento',
+  `validation_date` datetime NOT NULL COMMENT 'Fecha del Alta del Documento',
   `registration_ip` varchar(25) NOT NULL COMMENT 'IP del Alta del Documento',
   `activation_id` char(1) NOT NULL COMMENT 'Clave Foranea Activacions',
   `internalstate_id` char(1) NOT NULL COMMENT 'Clave Foranea Estadointernos',
@@ -261,42 +197,7 @@ CREATE TABLE IF NOT EXISTS `documents` (
   KEY `documentos_05` (`document_state_id`),
   KEY `documentos_03` (`user_id`),
   KEY `documentos_04` (`repository_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Documentos subidos por los Usuarios al Repositorio' AUTO_INCREMENT=42 ;
-
---
--- Volcado de datos para la tabla `documents`
---
-
-INSERT INTO `documents` (`id`, `name`, `description`, `register_date`, `register_ip`, `registration_date`, `registration_ip`, `activation_id`, `internalstate_id`, `user_id`, `repository_id`, `document_state_id`) VALUES
-(13, 'assaf', 'sadsfa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 8, 2),
-(14, 'asd', 'asdfsdg', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 8, 2),
-(15, 'dasfgsa', 'asfsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 8, 2),
-(16, 'hola', 'chao', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 8, 2),
-(17, 'safgsag', 'safasg', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(18, 'dsgds', 'gsdhes', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(19, 'asd', 'hjhjjh', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(20, 'dshds', 'gdsh', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(21, 'asaf', 'asfsagsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(22, 'sadgsa', 'gsafasg', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(23, 'sdfgsad', 'gsdhds', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(24, 'gdshdsggs', 'gsdgdsgsdgsd', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 10, 1),
-(25, 'asdags', 'gadgdafdag', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 8, 1),
-(26, 'safsag', 'sagsagsg', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 10, 8, 1),
-(27, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(28, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(29, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(30, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(31, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(32, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(33, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(34, 'sdafsa', 'asfdfgsa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(35, 'asdsaf', 'sadgas', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(36, 'asdsaf', 'sadgas', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 1, 13, 1),
-(37, 'asdasf', 'fasgsafag', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 10, 21, 1),
-(38, 'adsgasfasg', 'safsagsagsaas', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 10, 21, 1),
-(39, 'sdgasdfg', 'sgsafa', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 10, 21, 1),
-(40, 'fshqwqwqq', 'qqwweret', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 10, 21, 1),
-(41, 'ffsdxvcmvgf', 'sdfhzsgh', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', 'A', 'A', 10, 21, 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Documentos subidos por los Usuarios al Repositorio' AUTO_INCREMENT=140 ;
 
 -- --------------------------------------------------------
 
@@ -454,24 +355,7 @@ CREATE TABLE IF NOT EXISTS `repositories` (
   KEY `repositorios_01` (`activation_id`),
   KEY `repositorios_02` (`internalstate_id`),
   KEY `repositorios_03` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Repositorio de Documentos del Sistema' AUTO_INCREMENT=23 ;
-
---
--- Volcado de datos para la tabla `repositories`
---
-
-INSERT INTO `repositories` (`id`, `name`, `internal_name`, `description`, `created`, `modifed`, `activation_id`, `internalstate_id`, `user_id`) VALUES
-(8, 'example', 'example', 'esto es un example', '2012-04-30 02:42:16', NULL, 'A', 'A', 1),
-(10, 'example1', 'example1', 'sdafsa', '2012-05-01 03:35:36', NULL, 'A', 'A', 1),
-(11, 'example2', 'example2', 'sadfsa', '2012-05-05 02:44:57', NULL, 'A', 'A', 1),
-(12, 'sdafsa', 'asd', 'asasf', '2012-05-05 22:37:13', NULL, 'A', 'A', 1),
-(13, 'fsdhs', 'sdf', 'gasgha', '2012-05-05 22:37:24', NULL, 'A', 'A', 1),
-(14, 'asdf', 'as12', 'sdgsdh', '2012-05-05 22:37:41', NULL, 'A', 'A', 1),
-(15, 'asdf12', 'assagsa', 'safag', '2012-05-05 23:04:53', NULL, 'A', 'A', 10),
-(16, '12', '2233', '1233', '2012-05-05 23:15:44', NULL, 'A', 'A', 1),
-(17, '12122', '22331', '1233', '2012-05-05 23:19:42', NULL, 'A', 'A', 1),
-(21, 'myrepo1', 'myrepo1', 'myrepo1', '2012-05-07 02:10:46', NULL, 'A', 'A', 10),
-(22, 'dsfasg', 'sadsag', 'asdgsa', '2012-05-07 23:02:28', NULL, 'A', 'A', 11);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Repositorio de Documentos del Sistema' AUTO_INCREMENT=52 ;
 
 -- --------------------------------------------------------
 
@@ -486,24 +370,13 @@ CREATE TABLE IF NOT EXISTS `repositories_users` (
   `user_id` int(6) NOT NULL COMMENT 'Clave Foranea Usuarios',
   `repository_id` int(6) NOT NULL COMMENT 'Clave Foranea Repositorios',
   `user_type_id` int(2) NOT NULL COMMENT 'Clave Foranea Tipo_Usuarios',
-  `watching` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `usuario_repositorios_01` (`activation_id`),
   KEY `usuario_repositorios_02` (`internalstate_id`),
   KEY `usuario_repositorios_03` (`user_id`),
   KEY `usuario_repositorios_04` (`repository_id`),
   KEY `usuario_repositorios_05` (`user_type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
-
---
--- Volcado de datos para la tabla `repositories_users`
---
-
-INSERT INTO `repositories_users` (`id`, `activation_id`, `internalstate_id`, `user_id`, `repository_id`, `user_type_id`, `watching`) VALUES
-(1, 'N', 'A', 10, 11, 2, 0),
-(4, 'A', 'A', 10, 10, 2, 0),
-(6, 'A', 'A', 10, 21, 1, 0),
-(7, 'A', 'A', 11, 22, 1, 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
 
 -- --------------------------------------------------------
 
@@ -514,8 +387,8 @@ INSERT INTO `repositories_users` (`id`, `activation_id`, `internalstate_id`, `us
 CREATE TABLE IF NOT EXISTS `repository_restrictions` (
   `id` int(8) NOT NULL AUTO_INCREMENT COMMENT 'Identificador',
   `name` varchar(100) NOT NULL COMMENT 'Nombre de la Restriccion',
-  `extension` varchar(10) NOT NULL COMMENT 'Extension del Archivo que se Permite Subir al Repositorio',
-  `seize` int(10) NOT NULL COMMENT 'Peso Permitido del Archivo Adjunto',
+  `extension` varchar(100) NOT NULL COMMENT 'Extension del Archivo que se Permite Subir al Repositorio',
+  `size` int(10) NOT NULL COMMENT 'Peso Permitido del Archivo Adjunto',
   `amount` int(5) NOT NULL COMMENT 'Cantidad de Archivos Adjuntos Permitidos',
   `register_date` datetime NOT NULL COMMENT 'Fecha de Creacion de la Restriccion',
   `register_ip` varchar(25) NOT NULL COMMENT 'IP de Creacion de la Restriccion',
@@ -528,7 +401,7 @@ CREATE TABLE IF NOT EXISTS `repository_restrictions` (
   KEY `repositorio_restriccions_02` (`internalstate_id`),
   KEY `repositorio_restriccions_03` (`user_id`),
   KEY `repositorio_restriccions_04` (`repository_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -589,19 +462,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `users_01` (`activation_id`),
   KEY `users_02` (`internalstate_id`),
   KEY `users_03` (`is_administrator`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Usuarios del Sistema' AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Usuarios del Sistema' AUTO_INCREMENT=21 ;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `salt`, `is_administrator`, `activation_id`, `internalstate_id`) VALUES
-(1, 'hola', 'admin@example.com', 'admin', 'f62cb1c950869bd7e5c0fa7aff9292cf5e868856', '979830690', '1', 'A', 'A'),
-(5, 'asd', 'asdf@asdf.com', 'adminas', 'fbeb768f39f4487d2815a92ba9a9ac25913d1434', '395944676', '', 'A', 'A'),
-(7, 'qwerre', 'jkkj@jkjk.com', 'lkmnjn', 'addf20db054ee4fcd081d5f065f3bec11699e434', '1837267016', '0', 'A', 'A'),
-(8, 'yuirih', 'gh@hg.com', 'mnbv', 'd637b8f63a25286463de97a92d2a3f80263d10b5', '1022373586', '1', 'A', 'A'),
-(10, 'HOLA', 'admin@example1.com', 'fdhhdg', '5e69d71643e55ddc1d933a77d75e40b624cea8c3', '1652522636', '0', 'A', 'A'),
-(11, 'safsa', 'sda@dsaf.com', 'sadfas', 'd101f43845c0a4c8befe60a2657f1957b7570bf4', '1135013228', '0', 'A', 'A');
+(1, 'Superman', 'super@admin.com', 'superadmin', 'c61aff6f3eefb6f02e153656e98c85719607a9ab', '972881407', '1', 'A', 'A'),
+(2, 'Jhon Doe', 'user@example.com', 'johndoe', 'c4c257a92459ea0d62e8f07dcdd3ab7e39dd618e', '1424764266', '0', 'A', 'A');
 
 -- --------------------------------------------------------
 
